003c800d33738cd2ccb2696e3be730756855c2f9
Norbertas M <norbertas.mazeika@gmail.com>
2022-12-05 23:07:59 +0200

    Apply formatter rules

--- 2022/day-1/index.js
+++ 2022/day-1/index.js
@@ -1,21 +1,19 @@
-const fs = require('node:fs');
-const readline = require('node:readline');
-
+const fs = require("node:fs");
+const readline = require("node:readline");
 
 async function processLineByLine() {
-  const fileStream = fs.createReadStream('input');
+  const fileStream = fs.createReadStream("input");
   const rl = readline.createInterface({
     input: fileStream,
     crlfDelay: Infinity,
   });
 
-
-const elfs = [];
-let currentElf = [];
+  const elfs = [];
+  let currentElf = [];
   for await (const line of rl) {
-    if (line === ''){
+    if (line === "") {
       let currentElfTotal = 0;
-      currentElf.forEach(elfCalories => {
+      currentElf.forEach((elfCalories) => {
         currentElfTotal += elfCalories;
       });
       elfs.push(currentElfTotal);
@@ -31,7 +29,11 @@ let currentElf = [];
   elfs.reverse();
 
   console.log(`Elf carrying the most Calories carries ${elfs[0]}`);
-  console.log(`Top 3 elfs carrying the most Calories carries together ${elfs[0]+elfs[1]+elfs[2]}`);
+  console.log(
+    `Top 3 elfs carrying the most Calories carries together ${
+      elfs[0] + elfs[1] + elfs[2]
+    }`
+  );
 }
 
 processLineByLine();
