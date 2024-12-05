fb98190f508d4a4e5cef6275d72d0ffba597b128
Norbertas M. <norbertas.mazeika@gmail.com>
2024-12-05 06:23:34 +0000

    Fix formatting

--- 2024/day-1/distance.ts
+++ 2024/day-1/distance.ts
@@ -3,13 +3,13 @@ import { TextLineStream } from "jsr:@std/streams@1.0.8/text-line-stream";
 using file = await Deno.open("input");
 const readable = file.readable
   .pipeThrough(new TextDecoderStream())
-  .pipeThrough(new TextLineStream())
+  .pipeThrough(new TextLineStream());
 
-const firstArray: number[] = [], secondArray: number [] = [];
+const firstArray: number[] = [], secondArray: number[] = [];
 
-console.time('Time taken')
+console.time("Time taken");
 for await (const data of readable) {
-  const lineNumbers = data.split('   ');
+  const lineNumbers = data.split("   ");
   firstArray.push(parseInt(lineNumbers[0]));
   secondArray.push(parseInt(lineNumbers[1]));
 }
@@ -18,9 +18,9 @@ firstArray.sort();
 secondArray.sort();
 
 let totalDistance: number = 0;
-firstArray.forEach((value, index) =>{
-    totalDistance += Math.abs(value - secondArray[index])
-})
-console.timeEnd('Time taken')
+firstArray.forEach((value, index) => {
+  totalDistance += Math.abs(value - secondArray[index]);
+});
+console.timeEnd("Time taken");
 
 console.log(`Total distance: ${totalDistance}`);
--- 2024/day-1/similarity.ts
+++ 2024/day-1/similarity.ts
@@ -3,13 +3,13 @@ import { TextLineStream } from "jsr:@std/streams@1.0.8/text-line-stream";
 using file = await Deno.open("input");
 const readable = file.readable
   .pipeThrough(new TextDecoderStream())
-  .pipeThrough(new TextLineStream())
+  .pipeThrough(new TextLineStream());
 
-const firstArray: number[] = [], secondArray: number [] = [];
+const firstArray: number[] = [], secondArray: number[] = [];
 
-console.time('Time taken')
+console.time("Time taken");
 for await (const data of readable) {
-  const lineNumbers = data.split('   ');
+  const lineNumbers = data.split("   ");
   firstArray.push(parseInt(lineNumbers[0]));
   secondArray.push(parseInt(lineNumbers[1]));
 }
@@ -18,24 +18,24 @@ for await (const data of readable) {
 const cache: Record<number, number> = {};
 
 const countOccurances = (item: number) => {
-    if (!cache[item]) {
-        let count = 0;
-        secondArray.forEach(element=>{
-            if (element === item){
-                count += 1;
-            }
-        })
-        cache[item] = count;
-    }
-    return cache[item]
-}
+  if (!cache[item]) {
+    let count = 0;
+    secondArray.forEach((element) => {
+      if (element === item) {
+        count += 1;
+      }
+    });
+    cache[item] = count;
+  }
+  return cache[item];
+};
 
-let similarityScore = 0
+let similarityScore = 0;
 
-firstArray.forEach(element=>{
-    similarityScore += element * countOccurances(element)
-})
+firstArray.forEach((element) => {
+  similarityScore += element * countOccurances(element);
+});
 
-console.timeEnd('Time taken')
+console.timeEnd("Time taken");
 
-console.log(`Similarity score: ${similarityScore}`)
+console.log(`Similarity score: ${similarityScore}`);
