7c463b3edd979a0d2b3a85abfc11b52397f496b9
Norbertas M. <norbertas.mazeika@gmail.com>
2024-12-30 19:25:36 +0000

    Fix day 3 to contain both parts

--- 2024/day-3/memory-uncorrupter.ts
+++ 2024/day-3/memory-uncorrupter.ts
@@ -1,25 +1,51 @@
 const corruptedMemory = await Deno.readTextFile("input");
 
-console.time("Time taken");
-let result = 0;
-const multiRegex = /mul\(\d*,\d*\)|don't\(\)|do\(\)/g
-let currentState:'do'|'dont' = 'do'
+const calculatePart1 = (corruptedMemory: string) => {
+  let result = 0;
+  const multiRegex = /mul\(\d*,\d*\)/g;
 
-const allInstructions = [...corruptedMemory.matchAll(multiRegex)];
+  const allInstructions = [...corruptedMemory.matchAll(multiRegex)];
 
-allInstructions.forEach((instruction) => {
-  const instructionString = instruction.join();
-  if (instructionString.startsWith('do(')) {
-    currentState = 'do'
-  }
-  if (instructionString.startsWith('don\'t(')) {
-    currentState = 'dont'
-  }
-  if (instructionString.startsWith('mul') && currentState === 'do') {
-    const numbers = instructionString.replace('mul(', '').replace(')', '').split(',');
-    result += parseInt(numbers[0]) * parseInt(numbers[1]);
-  }
-});
+  allInstructions.forEach((instruction) => {
+    const instructionString = instruction.join();
+    if (instructionString.startsWith("mul")) {
+      const numbers = instructionString.replace("mul(", "").replace(")", "")
+        .split(",");
+      result += parseInt(numbers[0]) * parseInt(numbers[1]);
+    }
+  });
+  return result;
+};
+const calculatePart2 = (corruptedMemory: string) => {
+  let result = 0;
+  const multiRegex = /mul\(\d*,\d*\)|don't\(\)|do\(\)/g;
+  let currentState: "do" | "dont" = "do";
 
-console.timeEnd("Time taken");
-console.log(`Result ${result}`)
+  const allInstructions = [...corruptedMemory.matchAll(multiRegex)];
+
+  allInstructions.forEach((instruction) => {
+    const instructionString = instruction.join();
+    if (instructionString.startsWith("do(")) {
+      currentState = "do";
+    }
+    if (instructionString.startsWith("don't(")) {
+      currentState = "dont";
+    }
+    if (instructionString.startsWith("mul") && currentState === "do") {
+      const numbers = instructionString.replace("mul(", "").replace(")", "")
+        .split(",");
+      result += parseInt(numbers[0]) * parseInt(numbers[1]);
+    }
+  });
+  return result;
+};
+
+console.time("Time taken, part 1");
+const part1result = calculatePart1(corruptedMemory);
+console.timeEnd("Time taken, part 1");
+console.log(`Part 1 result: ${part1result}`);
+
+console.time("Time taken, part 2");
+const part2result = calculatePart2(corruptedMemory);
+console.timeEnd("Time taken, part 2");
+console.log(`Part 2 result: ${part2result}`);
